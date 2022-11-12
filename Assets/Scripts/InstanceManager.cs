000bc075303ebd07ebf30bd2c56f7113b97e9142
Thijs Rijpert <>
2022-11-12 16:50:35 +0100

    Fixed object clipping through the ground in the personal room

--- Assets/Scripts/InstanceManager.cs
+++ Assets/Scripts/InstanceManager.cs
@@ -3,18 +3,19 @@ using UnityEngine;
 public class InstanceManager : MonoBehaviour
 {
     [SerializeField] public string reference;
-    [SerializeField] public Material prefabColor;
+    [SerializeField] public string prefabColor;
 
     void Update()
     {
         if(Input.GetKeyUp(KeyCode.C))
         {
+            var material = Resources.Load<Material>("Materials/" + prefabColor);
             var prefab = Resources.Load<GameObject>("Prefaps/Interactable/" + reference);
             GameObject instance = Instantiate(prefab, Vector3.zero, Quaternion.identity);
             
             instance.transform.position = Vector3.one;
             
-            ColorChanger.FindMaterials(instance.transform, prefabColor);
+            ColorChanger.FindMaterials(instance.transform, material);
         }
     }
 }
