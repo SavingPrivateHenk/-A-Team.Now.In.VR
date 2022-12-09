8b6b19a5e0cbaf406fcbd50aa1912c8e72b40c5c
Stephan de Jongh <61140790+SJonguh@users.noreply.github.com>
2022-12-09 03:20:46 +0100

    Changes tooltip UI, added product price, increment/decrement buttons for adding higher quantities, and enlarged basket button for easier targeting.

--- Assets/Scripts/Basket/ProductTooltip.cs
+++ Assets/Scripts/Basket/ProductTooltip.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using TMPro;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
@@ -8,24 +11,43 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class ProductTooltip : MonoBehaviour
 {
     private GameObject m_tooltip;
-    private TextMeshProUGUI m_tooltipText;
-    private Button m_tooltipButton;
+
+    private TextMeshProUGUI m_productName;
+    private TextMeshProUGUI m_productPrice;
+    private TextMeshProUGUI m_productQuantity;
+
+    private Button m_basketButton;
+    private Button m_incrementButton;
+    private Button m_decrementButton;
+
     private ShoppingBasketManager m_shoppingBasketManager;
     private GameObject m_selectedGameObject;
     private Collider m_selectedObjectcollider;
 
+    private int m_tooltipQuantity;
+
     private void Awake()
     {
         m_tooltip = Instantiate(Resources.Load<GameObject>("Prefaps/Basket/Product Tooltip"));
-        m_tooltipText = m_tooltip.GetComponentInChildren<TextMeshProUGUI>();
-        m_tooltipButton = m_tooltip.GetComponentInChildren<Button>();
-        m_tooltipButton.onClick.AddListener(OnBasketButtonClick);
+
+        m_productName = m_tooltip.GetNamedChild("Productname").GetComponent<TextMeshProUGUI>();
+        m_productPrice = m_tooltip.GetNamedChild("Product Price").GetComponent<TextMeshProUGUI>();
+        m_productQuantity = m_tooltip.GetNamedChild("Product Quantity").GetComponent<TextMeshProUGUI>();
+
+        m_basketButton = m_tooltip.GetNamedChild("Basket Button").GetComponent<Button>();
+        m_incrementButton = m_tooltip.GetNamedChild("Increment Button").GetComponent<Button>();
+        m_decrementButton = m_tooltip.GetNamedChild("Decrement Button").GetComponent<Button>();
+
+        m_basketButton.onClick.AddListener(OnBasketButtonClick);
+        m_incrementButton.onClick.AddListener(() => UpdateQuantity(1));
+        m_decrementButton.onClick.AddListener(() => UpdateQuantity(-1));
+
         m_shoppingBasketManager = GetComponent<ShoppingBasketManager>();
     }
 
-    private void Update()
+    private void LateUpdate()
     {
-        m_tooltip.transform.LookAt(2 * m_tooltip.transform.position - Camera.main.transform.position);
+        m_tooltip.transform.LookAt(2 * m_tooltip.transform.position - Camera.main.transform.position + Camera.main.transform.rotation * Vector3.forward);
     }
 
     public void OnProductSelected(SelectEnterEventArgs args)
@@ -36,9 +58,11 @@ public class ProductTooltip : MonoBehaviour
         {
             m_selectedGameObject = interactableGameObject;
             m_selectedObjectcollider = m_selectedGameObject.GetComponent<Collider>();
-            m_tooltipText.text = product.ProductName;
-            m_tooltip.transform.SetParent(interactableGameObject.transform, false);
-            m_tooltip.transform.position = m_selectedObjectcollider.bounds.center + new Vector3(0f, m_selectedObjectcollider.bounds.extents.y + 0.2f, 0f);
+            m_productName.text = product.ProductName;
+            m_productPrice.text = product.ProductPrice.ToString("C", new CultureInfo("nl-NL"));
+            m_tooltipQuantity = 1;
+            m_productQuantity.text = m_tooltipQuantity.ToString();
+            m_tooltip.transform.position = m_selectedObjectcollider.bounds.center + new Vector3(0f, m_selectedObjectcollider.bounds.extents.y + 0.3f, 0f) - Camera.main.transform.forward;
             m_tooltip.SetActive(true);
         }
     }
@@ -46,9 +70,15 @@ public class ProductTooltip : MonoBehaviour
     private void OnBasketButtonClick()
     {
         var productBehaviour = m_selectedGameObject.GetComponent<ProductBehaviour>();
-        m_shoppingBasketManager.UpsertProduct(new Product(productBehaviour.ProductName, productBehaviour.ProductPrice, productBehaviour.PrefabName, productBehaviour.MaterialName), 1);
+        m_shoppingBasketManager.UpsertProduct(new Product(productBehaviour.ProductName, productBehaviour.ProductPrice, productBehaviour.PrefabName, productBehaviour.MaterialName), m_tooltipQuantity);
         m_tooltip.SetActive(false);
         m_selectedGameObject = null;
+    }
+
+    private void UpdateQuantity(int change)
+    {
+        m_tooltipQuantity = Math.Max(m_tooltipQuantity + change, 1);
+        m_productQuantity.text = m_tooltipQuantity.ToString();
     }
 
     private MonoBehaviour InteractableMonoBehaviour(IXRInteractable xrInteractable) => xrInteractable as MonoBehaviour;
